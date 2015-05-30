a090bfe0dcbb62b5901876d118cb894f98510519
UetaMasamichi <masamichi@Pony.local>
2015-05-30 14:44:31 +0900

    first commit

---
+++ CoreAnimation100DaysTests/CoreAnimation100DaysTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoreAnimation100DaysTests.swift
+//  CoreAnimation100DaysTests
+//
+//  Created by UetaMasamichi on 2015/05/30.
+//  Copyright (c) 2015年 Masamichi Ueta. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CoreAnimation100DaysTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
